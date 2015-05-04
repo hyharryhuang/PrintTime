29eeca5603180ca0674e714a7eb4b6c0bad424c5
Harry Huang <hy.harry.huang@gmail.com>
2015-05-04 12:27:32 +0800

    Initial commit

---
+++ print_time/printT.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def printT(message):
+	print(str(datetime.now()) + ': ' + message)
---
+++ setup.py
@@ -0,0 +1,18 @@
+setup(
+    name='print_time',
+    version='1',
+    description='Library to print python messages with a timestamp',
+    url='https://github.com/hyharryhuang/PrintTime',
+    packages=['print_time'],
+    author='Harry Huang',
+    maintainer='Harry Huang',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ]
+)
